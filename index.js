faaaddfb1020efa021e6e52f302dfe2bfed13e63
isfndiar <isfandiaradi21@gmail.com>
2024-04-20 00:46:00 +0700

    bismillah

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { app } from "./application/app.js";
+import { app } from "./src/application/app.js";
 app.listen(process.env.PORT, () => {
   console.log("Listening on port: ", process.env.PORT);
   // console.log(path.join(__dirname, './public/user-profile/profile'));
